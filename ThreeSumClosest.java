34e3fb5c498c8ae18358189baeb0ac9ab3bd5269
Abhishek Jain <ayushman.jain72@gmail.com>
2021-05-23 18:03:09 +0530

    3sum closest

---
+++ ThreeSumClosest.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class ThreeSumClosest {
+
+	public static void main(String[] args) {
+		ThreeSumClosest obj = new ThreeSumClosest();
+		System.out.println(obj.threeSumClosest(new int[] {-4, -2, -1, -1, 1, 2, 3}, 5));
+	}
+
+	public int threeSumClosest(int[] nums, int target) {
+		Arrays.sort(nums);
+		int k;
+		int j;
+		int minDiff = Integer.MAX_VALUE;
+
+		for(int i=0;i<nums.length-2;i++){
+			j = i+1;
+			k = nums.length-1;
+			while(j<k){
+				int sum = nums[i]+nums[j]+nums[k];
+				
+				if(sum == target)            
+					return sum;
+				else if(sum < target){
+					if(Math.abs(minDiff) > Math.abs(target-sum)) {
+						minDiff = target-sum;
+					}
+					j++;
+				}
+				else{
+					if(Math.abs(minDiff) > Math.abs(target-sum)) {
+						minDiff = target-sum;
+					}
+					k--;
+				}
+			}
+		}
+
+		return target-minDiff;
+	}
+}
